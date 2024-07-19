bef55a7ea0c146924083ab4da3898d953f7ed6a6
Marcelo <mhbcoelho99@gmail.com>
2024-07-19 17:11:56 -0300

    create cat-file command

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 import * as fs from 'node:fs'
 import path from 'node:path';
+import * as zlib from 'node:zlib';
 
 
-const command = process.argv[2];
-
-switch(command) {
+const command = process.argv;
+console.log(command)
+switch (command[2]) {
   case "init":
     init()
     break
+
+  case "cat-file":
+    if (command.length < 4) {
+      throw new Error('Usage: cat-file <sha1>');
+    }
+    catFile(command[3]);
+    break;
   default:
     throw new Error(`Unknown command ${command}`);
+} 
+
+
+function catFile(hash) {
+  const dir = path.join(process.cwd(), '.git', 'objects', hash.substring(0, 2))
+  const file = path.join(dir, hash.substring(2))
+
+  if(!fs.existsSync(file)) {
+    throw new Error(`Object ${hash} not found`)
+  }
+
+  const content = fs.readFileSync(path.join(dir, file))
+  const dataUnzipped = zlib.inflateSync(content)
+  const res = dataUnzipped.toString().split('\0')[1];
+  process.stdout.write(res);
 }
 
 function init() {
+  const gitDir = path.join(process.cwd(), '.git');
+  const headFile = path.join(gitDir, 'HEAD');
+
+  if (fs.existsSync(gitDir) && fs.existsSync(headFile)) {
+    console.log('Git repository already initialized');
+    return;
+  }
+
   fs.mkdirSync(path.join(process.cwd(), '.git'), { recursive: true });
   fs.mkdirSync(path.join(process.cwd(), ".git", "objects"), { recursive: true });
   fs.mkdirSync(path.join(process.cwd(), ".git", "refs"), { recursive: true });
